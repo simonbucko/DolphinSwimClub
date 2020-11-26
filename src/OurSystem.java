61181de7be68717a47e05f7715448cd5be917a52
mikke <mikkel.vermehren@gmail.com>
2020-11-26 12:20:51 +0100

    Looks better now

--- src/OurSystem.java
+++ src/OurSystem.java
@@ -3,5 +3,6 @@ public class OurSystem {
     public static void main(String[] args) {
         System.out.println("Ahoj");
         System.out.println("Mikkel");
+        System.out.println("Simon");
     }
 }
