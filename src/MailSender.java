b03f36ef8a6e06690209c859286e2537b338c761
iamsimon <simon.bucko.web@gmail.com>
2020-11-28 17:18:03 +0100

    edited runsystem

--- src/MailSender.java
+++ src/MailSender.java
@@ -1,2 +1,15 @@
+import java.util.Properties;
+
 public class MailSender {
+    public void sendMail(String receptient){
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth","true");
+        properties.put("mail.smtp.starttls.enable","true");
+        properties.put("mail.smtp.host","smtp.gmail.com");
+        properties.put("mail.smtp.port","587");
+        String EMAIL = "dolphin.club.system@gmail.com";
+        String PASSWORD = "dolphinclub123";
+
+
+    }
 }
--- src/RunSystem.java
+++ src/RunSystem.java
@@ -1,4 +1,5 @@
 public class RunSystem {
 
-
+    String samuel;
+    String vlad;
 }
