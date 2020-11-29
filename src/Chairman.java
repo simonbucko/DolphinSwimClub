5db4971304c18cb2f2280bdee970599a1a2c3040
Samuel <samu2451@stud.kea.dk>
2020-11-29 18:47:58 +0100

    Created chairman, will change

---
+++ src/Chairman.java
@@ -0,0 +1,17 @@
+public class Chairman {
+
+    private String password;
+
+    public String getPassword(){
+        return name;
+    }
+    public void setPassword(String newPassword){
+        this.name = newPassword;
+    }
+
+    Swimmer newSwimmer = new Swimmer();
+    Coach newCoach = new Coach();
+
+
+
+}
