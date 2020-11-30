b7e5cbea0639c0fe7571472197a989e01e983fb1
Samuel <samu2451@stud.kea.dk>
2020-11-30 20:00:11 +0100

    add swimmer and coach

--- src/Chairman.java
+++ src/Chairman.java
@@ -1,6 +1,7 @@
+import java.util.ArrayList;
 public class Chairman {
 
-//    private String password;
+//    private String password = "password";
 //
 //    public String getPassword(){
 //        return name;
@@ -8,10 +9,13 @@ public class Chairman {
 //    public void setPassword(String newPassword){
 //        this.name = newPassword;
 //    }
-//
-//    Swimmer newSwimmer = new Swimmer();
-//    Coach newCoach = new Coach();
-//
-//
+
+    public void addCoach(String name, String surname, int age, String swimmStyle, String password){
+        ArrayList<String> coachList = new ArrayList<>(name, surname, age, swimmStyle, password);
+        coachList.add(coach);
+    }
+    public void addSwimmer(String name, String surname, int age, String swimmStyle){
+        ArrayList<String> swimmerList = new ArrayList<>(name, surname, age, swimmStyle,);
+        swimmerList.add(swimmer);
 
 }
