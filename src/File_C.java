5ec284db83bfc9f5a2b85e4bd8244ec8333eb7b4
Samuel <samu2451@stud.kea.dk>
2020-12-02 10:20:08 +0100

    finished file C appart from find coach by id

--- src/File_C.java
+++ src/File_C.java
@@ -5,7 +5,7 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class File_C {
+public class File_c {
 
         public File file = null;
         public Scanner sc = null;
@@ -14,15 +14,29 @@ public class File_C {
 
     private ArrayList<Coach> coaches = new ArrayList<Coach>();
 
-        public File_C() throws IOException {
+        public createFile() throws IOException {
             file = new File("coaches.txt");
             if(!file.exists()) file.createNewFile();
             fw = new FileWriter(file,true);
             pw = new PrintWriter(fw,true);
         }
 
+    public void loadData() throws IOException{
+        sc = new Scanner(file);
+        sc.nextLine();
+        while(sc.hasNext()){
+            int id = sc.nextInt();
+            String name = sc.next();
+            String surName = sc.next();
+            int age = sc.nextInt();
+            int phone = sc.nextInt();
+            String email = sc.next();
+            String swimStyle = sc.next();
+        }
+    }
 
-        public void eraseFile() throws IOException{
+
+    public void eraseFile() throws IOException{
             FileWriter fw = new FileWriter(file,false);
             PrintWriter pw = new PrintWriter(fw,true);
             pw.print("");
@@ -30,7 +44,7 @@ public class File_C {
 
     public void printAllData(){
         for (Coach coach : coaches){
-            //System.out.println(swimmer.getId() + " " + swimmer.getName() + " " + swimmers.getSurName() + " " + swimmers.getAge() + " " + swimmers.getPhone() + " " + swimmers.getEmail() + " "+ swimmers.getMembershipStart() + " " + swimmers.getMembershipEnd() + " " + swimmers.getType() + " " + swimmers.getSwimStyle() + " " + swimmers.getWasNotified() + " ");
+            System.out.println(coach.getId() + " " + coach.getName() + " " + coach.getSurName() + " " + coach.getAge() + " " + coach.getPhone() + " " + coach.getEmail() + " " + coach.getSwimStyle() + " + ");
         }
     }
 
@@ -38,24 +52,26 @@ public class File_C {
         coaches.remove(index);
         eraseFile();
         for(Coach coach : coaches){
-            //System.out.println(swimmer.getId() + " " + swimmer.getName() + " " + swimmers.getSurName() + " " + swimmers.getAge() + " " + swimmers.getPhone() + " " + swimmers.getEmail() + " "+ swimmers.getMembershipStart() + " " + swimmers.getMembershipEnd() + " " + swimmers.getType() + " " + swimmers.getSwimStyle() + " " + swimmers.getWasNotified() + " ");
+            System.out.println(coach.getId() + " " + coach.getName() + " " + coach.getSurName() + " " + coach.getAge() + " " + coach.getPhone() + " " + coach.getEmail() + " " + coach.getSwimStyle() + " + ")
         }
 
     }
 
-        public void loadData() throws IOException{
-            sc = new Scanner(file);
-            sc.nextLine();
-            while(sc.hasNext()){
-                int id = sc.nextInt();
-                String name = sc.next();
-                String surName = sc.next();
-                int age = sc.nextInt();
-                int phone = sc.nextInt();
-                String email = sc.next();
-                String swimStyle = sc.next();
-            }
+    public void saveToFile(int id, String name, String surname, int age, int phone, String email, String swimStyle){
+        try {
+            eraseFile();
         }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        prepareForWriting();
+        printHeader();
+
+        pw.println(id + " " + name + " " + surname + " " + age + " " + phone + " " + email + " " + swimStyle + " " );
+        pw.close();
+
+    }
 
     public void prepareForWriting(){
         try{
@@ -96,23 +112,15 @@ public class File_C {
         pw.println();
     }
 
-//    public void getCoachById(){
-//    for(Coach coach : List) {
-//        if(account.getId().equals(someId) {
-//            //found it!
-//            }
-//        }
-//    }
-//
-//    public void getCoachById(String anId)
-//    {
-//        int arraySize = coaches.size();
-//        for(int i=0; i<arraySize; i++) {
-//            if (coaches.get(i).getId().equals(anId)) {
-//                System.out.println(coaches.get(i));
-//            }
-//        }
-//    }
+    public void getCoachById(String anId)
+    {
+        int arraySize = coaches.size();
+        for(int i=0; i<arraySize; i++) {
+            if (coaches.get(i).getId().equals(anId)) {
+                System.out.println(coaches.get(i));
+            }
+        }
+    }
 }
 
 
